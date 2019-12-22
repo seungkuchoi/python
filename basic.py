866748694d972831f5a660f64bfd0ceb183f126b
tmdrnahs <37217634+tmdrnahs@users.noreply.github.com>
2019-12-22 23:18:32 +0900

    list, dictionary, tuple

--- basic.py
+++ basic.py
@@ -19,28 +19,3 @@ print('{}, {}'.format(x,y))
 # input function
 res = input('input: ')
 print(res)
-
-# list
-list1 = [1, 2, 3]
-list1.append(4)
-print(list1[-1]) # last element
-list2 = [5,6,7]
-list3 = list1 + list2
-print(list3)
-
-print(4 in list1)
-del list1[0]
-print(list1)
-list1.remove(2)
-print(list1)
-
-# for
-for ele in list3:
-    print(ele, end=' ')
-
-for ele in range(len(list3)):
-    print(ele)
-
-state = ['a', 'b', 'c']
-for i, ele in enumerate(state):
-    print("""{}.'{}'""".format(i+1, ele))
