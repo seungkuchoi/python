99f628885d1c0626e82cfec10995f727668a1347
tmdrnahs <37217634+tmdrnahs@users.noreply.github.com>
2020-02-02 01:17:06 +0900

    Add date widgets and scheduler widget

--- toy_project/calendar/my_calendar.py
+++ toy_project/calendar/my_calendar.py
@@ -1,11 +1,11 @@
 
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton
 from PyQt5.QtCore import Qt
 
 from datetime import datetime
 from calendar import Calendar
 
-class _UserData():
+class UserData():
     """
     This class is responsible for extracting user data from 'setting.json'
     """
@@ -22,36 +22,64 @@ class _UserData():
         for index, weekday in enumerate(weekday_list):
             if weekday == self._json_object["firstWeekday"]:
                 return index
-    
+
     def write(self, key, value):
         self._json_object[key] = value
 
-class _DateInfo(Calendar):
+user_data = UserData('settings.json')
+
+class DateInfo(Calendar):
     def __init__(self):
-        self._user_data = _UserData('settings.json')
-        self.setfirstweekday(self._user_data.first_weekday)
+        self.setfirstweekday(user_data.first_weekday)
 
         today = datetime.today()
         self.year = today.year
         self.month = today.month
         self.day = today.day
-        self.week = 0
+        self.week = today.isocalendar()[1] % 5
 
-        date_list = self.monthdatescalendar(self.year, self.month)
-        if self._user_data.view_type == 'monthly':
-            self.date_list = date_list
+    def get_dates(self):
+        if user_data.view_type == 'monthly':
+            return self.monthdatescalendar(self.year, self.month)
         else:
-            self.date_list = date_list[self.week]
+            return self.monthdatescalendar(self.year, self.month)[self.week]
 
     def show(self):
-        for index, date in enumerate(self.date_list):
-            print(index, date)
+        print(self.get_dates())
+
+date_info = DateInfo()
+#date_info.show()
+
+class Scheduler(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, flags=Qt.Widget)
+        self.init_widget()
+
+    def init_widget(self):
+        self.setGeometry(800, 400, 300, 150)
+        textLabel = QPushButton("scheduler", self)
+        textLabel.move(20, 20)
 
 class MyCalendar(QWidget):
     def __init__(self):
+        self.scheduler = Scheduler()
         QWidget.__init__(self, flags=Qt.Widget)
-        self.date_info = _DateInfo()
-        self.date_info.show()
+        self.init_widget()
 
     def init_widget(self):
         self.setWindowTitle("Seungku 캘린더")
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        row = 5
+        col = 7
+
+        for x in range(0, row):
+            for y in range(0,col):
+                date_button = QPushButton(str(date_info.get_dates()[x][y].day), self)
+                date_button.clicked.connect(self.show_scheduler)
+                layout.addWidget(date_button, x, y)
+
+    def show_scheduler(self):
+        self.scheduler.show()
+
