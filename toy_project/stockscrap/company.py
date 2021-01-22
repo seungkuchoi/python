d024d7c00d717592b714cb7eba1c0b5aadffb266
tmdrnahs <37217634+tmdrnahs@users.noreply.github.com>
2021-01-23 00:00:14 +0900

    Change basecamp & Impl funcs
    1. base website change:
    - fnguide -> naver
    2. Add funcs for info, price, consensus

--- toy_project/stockscrap/company.py
+++ toy_project/stockscrap/company.py
@@ -9,37 +9,98 @@ company_info_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?me
 company_info_df.종목코드 = company_info_df.종목코드.map('{:06d}'.format)
 # company_info_df = configDataFrame({'회사명':'name', '종목코드':'code'})
 
-def configDataFrame(item):
+def configDataFrame(item, df=company_info_df):
     if type(item) is list:
-        return company_info_df[item]
+        return df[item]
     elif type(item) is dict:
-        company_code_df = company_info_df[item.keys()]
+        company_code_df = df[item.keys()]
         return company_code_df.rename(columns=item)
-    return company_info_df[item]
+    return df[item]
 
-def getCompanyInfo(name, column, df=company_info_df):
-    value = df.query("회사명=='{}'".format(name))[column]
+def getCompanyInfo(name, df=company_info_df):
+    value = df.query("회사명=='{}'".format(name))
     # To Do: query 이후 row 반환되면 list일텐데 자료형 다루는 방법 고민
-    return value.to_string(index=False).strip()
+    if value.empty is True:
+        raise ValueError
+    return value
 
 def getCompanySource(code):
-    base_url = 'https://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&cID=&MenuYn=Y&gicode=A'
+    base_url = 'https://finance.naver.com/item/main.nhn?code='
     response = requests.get(base_url + code)
     return response.text
 
+def num(text) -> int or float:
+    num = text.replace(',','').replace('%','')
+    if '.' in num:
+        return float(num)
+    return int(num)
+
 class Company():
     def __init__(self, name):
-        code = getCompanyInfo(name, '종목코드')
-        source = getCompanySource(code)
-        self.bs_obj = BeautifulSoup(source, 'lxml')
+        self.name = name
+        try:
+            company_info = getCompanyInfo(self.name) #xlxs type
+        except ValueError:
+            print('Invalid company name')
+        else:
+            self.code = company_info['종목코드'].to_string(index=False).strip()
+            source = getCompanySource(self.code)
+            self.bs_obj = BeautifulSoup(source, 'lxml')
+        finally:
+            self.info = self.parseInfo()
+            self.price = self.parsePrice()
+            self.consensus = self.parConsensus()
 
     def __str__(self):
-        '''print(company)'''
-        company_info = self.bs_obj.find('div', {'class':'ul_corpinfo'})
-        name = company_info.find('h1').text
-        code = company_info.find('h2').text
-        return 'name: {}, code: {}'.format(name, code)
+        '''print(stock_info)'''
+        return '{}'.format(self.name)
 
-    def getPrice(self):
-        price = self.bs_obj.find('span', {'id':'svdMainChartTxt11'})
-        return price.text
+    def parseInfo(self, item=None):
+        info = {'name':'', 'code':'', 'market':''}
+        try:
+            wrap_company = self.bs_obj.find('div', {'class':'wrap_company'})
+            name = wrap_company.find('h2')
+
+            description = wrap_company.find('div', {'class':'description'})
+            code = description.find('span', {'class':'code'})
+            market = description.find('img')['alt']
+
+            info = {'name':name.text, 'code':code.text, 'market':market}
+        finally:
+            return info
+
+    # 한번 이상 호출되면 parse 다른 곳으로 이동
+    def parsePrice(self, item=None):
+        price = {'close':0,'diff_prev':0,'rate_prev':0}
+        try:
+            price_info = self.bs_obj.find('div', {'class':'rate_info'})
+
+            today = price_info.find('p', {'class':'no_today'})
+            close = today.find('span', {'class':'blind'}) #종가
+
+            yesterday = price_info.find('p', {'class':'no_exday'}).findAll('em')
+            diff_prev = yesterday[0].find('span', {'class':'blind'}) #전일비
+            rate_prev = yesterday[1].find('span', {'class':'blind'}) #전일비%
+            
+            up_down = '+'
+            if yesterday[0].find('span', {'class':'ico down'}):
+                up_down = '-'
+
+            price = {'close':num(close.text),
+                    'diff_prev':num(up_down + diff_prev.text),
+                    'rate_prev':num(up_down + rate_prev.text)}
+        finally:
+            return price
+
+    def parConsensus(self): #투자 정보
+        consensus = {'invest_opinion':0, 'target_price':0}
+        try:
+            invest_info = self.bs_obj.find('div', {'class':'aside_invest_info'})
+            table = invest_info.find('table', {'class':'rwidth'}).findAll('td')
+            invest_opinion = num(table[0].findAll('em')[0].text)
+            target_price = num(table[0].findAll('em')[1].text)
+
+            consensus = {'invest_opinion':invest_opinion, 
+                        'target_price':target_price}
+        finally:
+            return consensus
--- toy_project/stockscrap/main.py
+++ toy_project/stockscrap/main.py
@@ -1,5 +1,7 @@
 from company import Company
 
-comp = Company('LG전자')
-print(comp)
-print('price: {}'.format(comp.getPrice()))
+lge = Company('LG전자')
+print(lge)
+print('CompanyInfo: {}'.format(lge.info))
+print('Price: {}'.format(lge.price))
+print('Consensus: {}'.format(lge.consensus))
